ecc57a77d06e09d069f6378c1d67caaa7d4a8c44
Hafid <hafid@MacBook-Air.local>
2026-05-11 16:19:01 +0200

    Add ChevreEtChoux game

---
+++ ChevreEtChoux/chevre-choux/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BoardComponent } from './components/board/board.component';
+import { ConfigurationMenuComponent } from './components/configuration-menu/configuration-menu.component';
+import { CreditComponent } from './components/credit/credit.component';
+import { GameModeMenuComponent } from './components/game-mode-menu/game-mode-menu.component';
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'game-mode-selection'},
+  {path: 'configuration', component: ConfigurationMenuComponent},
+  {path: 'game-mode-selection', component: GameModeMenuComponent},
+  {path: 'board', component: BoardComponent},
+  {path: 'credit', component: CreditComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ ChevreEtChoux/chevre-choux/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ConfigurationMenuComponent } from './components/configuration-menu/configuration-menu.component';
+import { GameModeMenuComponent } from './components/game-mode-menu/game-mode-menu.component';
+import { GameService } from './services/game/game.service';
+import { FormsModule } from '@angular/forms';
+import { CreditComponent } from './components/credit/credit.component';
+import { BoardComponent } from './components/board/board.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ConfigurationMenuComponent,
+    GameModeMenuComponent,
+    BoardComponent,
+    CreditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    GameService,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
